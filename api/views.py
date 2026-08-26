ed9ba5ba995120da97c6579e912827f2a3a75861
Camilo <inge.camil@gmail.com>
2026-08-25 22:51:30 -0500

    mi rest commit

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .models import Producto
+from .serializer import productoSerializer
+
+# Create your views here.
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = productoSerializer
